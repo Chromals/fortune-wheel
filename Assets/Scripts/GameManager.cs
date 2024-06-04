5319e9ab36525579bd78ac5ac9959460e8df1b3f
Chromals <camposcespedesd@gmail.com>
2024-06-03 21:23:43 -0600

    Clase semana 3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,42 +13,92 @@ public class GameManager : MonoBehaviour
     [SerializeField, Tooltip("total amount of winnings from player")]
     TextMeshProUGUI winningsTextbox;
 
+    [SerializeField, Tooltip("fortune wheel")]
+    Transform fortuneWheel;
+
+    [SerializeField, Tooltip("Spin button")]
+    Button spinButton;
+
     [Space]
     [SerializeField, Tooltip("Number of times the player can spin the wheel")] //esto lo que hace es que se pueda ver en el unity
     int turns;
 
-    [SerializeField, Tooltip("Number of full spins the wheel must complete before stopping")] 
+    [SerializeField, Tooltip("Number of full spins the wheel must complete before stopping")]
     int cycles;
 
+    [SerializeField, Tooltip("Number of seconds the wheel can spin")]
+    float maxSpinningTime;
+
+    private float _startAngle;
+    private float _finalAngle;
+    private bool _isSpinning;
+    private float _currentSpinningTime;
+
     private float _winnings;
-    private float[] _angles = new float[] { 22.5F, 67.5F,112.5F,157.5F,202.5F,247.5F,292.5F,337.5F };
+    private float[] _angles = new float[] { 1.0F, 46.0F, 91.0F, 136.0F, 181.0F, 226.0F, 261.0F, 306.0F };
 
 
     private void Awake()
     {
         turnsTextBox.text = turns.ToString();
-        winningsTextbox.text = _winnings.ToString("0.00");
+        winningsTextbox.text = "$" + _winnings.ToString("0.00");
     }
-    /*
-    private void Start()
-    {
 
+    public void Spin()
+    {
+        spinButton.interactable = false;
+        float randomFinalAngle = _angles[Random.Range(0, _angles.Length)];
+        _finalAngle = -(cycles * 360 + randomFinalAngle);
+        _currentSpinningTime = 0.0F;
+        _isSpinning = true;
     }
 
     private void Update()
     {
         //este usa delta time
+        if (!_isSpinning)
+            return;
+
+        _currentSpinningTime += Time.deltaTime;
+
+        bool isTimeout = _currentSpinningTime > maxSpinningTime;
+        bool hasReachedFinalAngle = fortuneWheel.eulerAngles.z == _finalAngle;
+        if (isTimeout || hasReachedFinalAngle)
+        {
+            _currentSpinningTime = maxSpinningTime;
+            _startAngle = _finalAngle % 360;
+            fortuneWheel.eulerAngles = new Vector3(0.0F, 0.0F, _startAngle);
+            _isSpinning = false;
+            spinButton.interactable = true;
+            return;
+        }
+
+
+        float t = _currentSpinningTime / maxSpinningTime;
+        t = Mathf.Pow(t, 3.0F) * (t * (4.0F * t - 10.0F) + 8.0F);
+
+        float angle = Mathf.Lerp(_startAngle, _finalAngle,t);
+        fortuneWheel.eulerAngles = new Vector3(0.0F,0.0F,angle);
     }
 
-    private void FixedUpdate()
+    
+    private void Start()
     {
-        //este usa fixedDeltaTime
+        _startAngle = _angles[Random.Range(0,_angles.Length)];
+        fortuneWheel.eulerAngles = new Vector3(0.0F, 0.0f, _startAngle);
     }
 
-    private void LateUpdate()
-    {
-        //este usa delta time y se ejecuta despues del Update y el FixedUpdate
-    }
+    /*
 
-    */
+     private void FixedUpdate()
+     {
+         //este usa fixedDeltaTime
+     }
+
+     private void LateUpdate()
+     {
+         //este usa delta time y se ejecuta despues del Update y el FixedUpdate
+     }
+
+     */
 }
